ce57bfe1b5345847319e06adad4784f2688c112b
Michael Mason <mwmason1998@gmail.com>
2022-06-07 10:58:04 -0400

    Daily Algo 7 June 2022

---
+++ C#/average_sll.js
@@ -0,0 +1,25 @@
+class Node{
+    constructor(data){
+        self.next = null;
+        self.data = data;
+    }
+}
+class SLL{
+    constructor(){
+        self.head = null;
+    }
+    // Given a value, insert that value as a node at the front of your singly linked list
+    insertAtFront(val) {
+        // your code here
+    }
+    
+    // Remove and return the head node from your list (remember this means we need a new head)
+    removeHead(){
+        // your code here
+    }
+    
+    // EXTRA: calculate the average of all the values in your list (ex: if you list contained the values 3, 5, 2, 7, 3, then your average should come out as 4)
+    average(){
+        // your code here
+    }
+}
