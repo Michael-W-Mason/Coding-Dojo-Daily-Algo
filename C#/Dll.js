0f4e4b8c81cbb5ac8232ffccb1c74c175ec75c0b
Michael Mason <mwmason1998@gmail.com>
2022-06-24 11:12:05 -0400

    Daily Algo 24 June 2022

--- C#/Dll.js
+++ C#/Dll.js
@@ -41,20 +41,20 @@ class Dll {
     removeMiddle() {
         let start = this.head;
         let end = this.tail;
-        if(!start && !end){
+        if (!start && !end) {
             return this;
         }
-        else if(start.next == end){
+        else if (start.next == end) {
             this.head = this.tail;
             this.tail.prev = null;
             return this;
         }
-        else if(start == end){
+        else if (start == end) {
             this.head = null;
             this.tail = null;
             return this;
         }
-        while(start !== end){
+        while (start !== end) {
             start = start.next;
             end = end.prev;
         }
@@ -63,14 +63,92 @@ class Dll {
         return this;
     }
 
+    insertAfter(search, val) {
+        let runner = this.head;
+        while (runner) {
+            if (runner.data === search) {
+                break;
+            }
+            runner = runner.next;
+        }
+        if (!runner) {
+            return "Value does not exist";
+        }
+        let node = new Node(val);
+        if (runner === this.tail) {
+            runner.next = node;
+            node.prev = runner;
+            this.tail = node;
+        }
+        else {
+            node.next = runner.next;
+            node.prev = runner;
+            runner.next.prev = node;
+            runner.next = node;
+        }
+        return this;
+    }
+
+    insertBefore(search, val) {
+        let runner = this.head;
+        while (runner) {
+            if (runner.data === search) {
+                break;
+            }
+            runner = runner.next;
+        }
+        if (!runner) {
+            return "Value does not exist";
+        }
+        let node = new Node(val);
+        if (runner === this.head) {
+            node.next = runner;
+            runner.prev = node;
+            this.head = node;
+        }
+        else {
+            node.next = runner;
+            node.prev = runner.prev;
+            runner.prev.next = node;
+            runner.prev = node;
+        }
+        return this;
+    }
+
+    reverse() {
+        let temp = null;
+        let start = this.head;
+        let end = this.tail;
+        if(!start){
+            return this;
+        }
+        while(start !== end){
+            if(start.next === end){
+                break;
+            }
+            temp = start.data;
+            start.data = end.data;
+            end.data = temp;
+            start = start.next;
+            end = end.prev;
+        }
+        return this; 
+    }
+
     print() {
         let arr = [];
+        let arr2 = [];
         let runner = this.head;
         while (runner) {
             arr.push(runner.data);
             runner = runner.next;
         }
-        console.log(arr);
+        runner = this.tail;
+        while (runner) {
+            arr2.push(runner.data);
+            runner = runner.prev;
+        }
+        console.log(arr, arr2);
     }
 }
 
@@ -78,10 +156,9 @@ let dll = new Dll();
 dll.insertAtBack(1);
 dll.insertAtBack(2);
 dll.insertAtBack(3);
+dll.insertAtBack(4);
+dll.insertAtBack(5);
 dll.print();
-dll.removeMiddle();
+dll.reverse();
 dll.print();
-dll.removeMiddle();
-dll.print();
-dll.removeMiddle();
-dll.print();
+console.log(dll.head.data, dll.tail.data);
