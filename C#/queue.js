e511b6dbb1be7720039a68926bc2a3a30cfe139d
Michael Mason <mwmason1998@gmail.com>
2022-06-29 10:49:27 -0400

    Daily Algo 29 June 2022

--- C#/queue.js
+++ C#/queue.js
@@ -19,12 +19,15 @@ class Queue{
         if(this.isEmpty()){
             this.tail = node;
             this.top = node;
-            return this;
+            this.size++
+            return this.size;
         }
         else{
             this.tail.next = node;
             this.tail = node;
-            return this;
+            this.size++
+            return this.size;
+
         }
     }
     
@@ -37,6 +40,7 @@ class Queue{
             let temp = this.top;
             this.top = this.top.next;
             temp.next = undefined;
+            this.size--;
             return temp;
         }
     }
@@ -62,14 +66,77 @@ class Queue{
         }
         console.log(str);
     }
+
+
+    compare(queue) {
+        let runner1 = this.top;
+        let runner2 = queue.top;
+        while(runner1 || runner2){
+            try{
+                if(runner1.data !== runner2.data){
+                    return false;
+                }
+            }
+            catch{
+                return false;
+            }
+            runner1 = runner1.next;
+            runner2 = runner2.next;
+        }
+        return true;
+    }
+
+
+    isPalidrome() {
+        let hash = {};
+        let count = Math.floor(this.size / 2);
+        let runner = this.top;
+        let i = 0;
+        if(this.isEmpty()){
+            return true;
+        }
+        // Generate Hash Map up to first half of queue
+        while(i < count){
+            hash[i] = runner.data;
+            runner = runner.next;
+            i++;
+        }
+        // Move iterator back one so its at a valid point on our hash
+        i--;
+        // If our queue is odd then we want to ignore the middle value because it does not need to be compared to anything
+        if(this.size % 2 !== 0){
+            runner = runner.next;
+        }
+        // Continue through second half of our queue and compare it to first half stored in the hash
+        while(runner){
+            if(hash[i] === runner.data){
+                i--;
+                runner = runner.next;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 let myQueue = new Queue();
 myQueue.enqueue(1);
 myQueue.enqueue(2);
 myQueue.enqueue(3);
+// myQueue.enqueue(3);
+// myQueue.enqueue(2);
+// myQueue.enqueue(1);
 myQueue.Log();
-myQueue.dequeue();
-myQueue.Log();
-console.log(myQueue.front());
-console.log(myQueue.top.data, myQueue.tail.data);
+
+// let myQueue2 = new Queue();
+// myQueue2.enqueue(1);
+// myQueue2.enqueue(2);
+// myQueue2.enqueue(3);
+
+// let temp = myQueue;
+// temp.top.next = undefined;
+// myQueue.Log();
+
+console.log(myQueue.isPalidrome());
